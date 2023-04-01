b798fed8b425081bd4b790aec91e8c09a10a6193
Indira Kumar <indirakumarak@gmail.com>
2023-04-01 21:11:24 +0530

    add Auth tests

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,77 +3,83 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import { registerValidation, loginValidation } from "../utils/validation.js";
 
+//Indira original version
 export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
   //Validating data before user creation
-  const { error } = registerValidation(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
+  const error = registerValidation(req.body);
+  if (error[0].msg) {
+    return res.status(400).send(error[0].msg);
   }
-  else {
-    User.findOne({ email: req.body.email })
-      .then((user) => {
-        if (user) {
-          return res.status(409).send(error.details[0].message);
-        }
-        else {
-          return bcrypt.genSalt(10);
-        }
-      })
-      .then((salt) => {
-        return bcrypt.hash(req.body.password, salt);
-      })
-      .then((hash) => {
-        const user = new User({
-          name: req.body.name,
-          email: req.body.email,
-          password: hash,
-        });
-        return user.save();
-      })
-      .then((user) => {
-        res.send({ user: user._id, message: "User created" });
-      })
-      .catch((err) => console.log(err));
+
+  //checking whether the user already exists in the database
+  try{
+    const emailExist = await User.findOne({ email });
+  if (emailExist) {
+    return res.send("Email already exists");
+  }
+  }catch(err){
+    console.log(err);
+  }
+
+  try{
+      //Hashing the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //creating a new user
+  const user = new User({
+    name: name,
+    email: email,
+    password: hashedPassword,
+  });
+    
+    const savedUser = await user.save();
+    res.send({ user: savedUser._id, message: "User created" });
+  } catch (err) {
+    res.status(400).send(err);
   }
 };
 
+//Indira's login version
 export const login = async (req, res) => {
+try{
+  const { email, password } = req.body;
 
   //Validating data before cross checking credentials
-  const { error } = loginValidation(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
+  const error = loginValidation(req.body);
+  if (error[0].msg) {
+    return res.status(400).send(error[0].msg);
   }
-  else {
-    //checking whether the user already exists in the database
-    User.findOne({ email: req.body.email })
-      .then((user) => {
-        if (!user) {
-          return res.status(401).send("Invalid email or password");
-        }
-        else {
-          const isMatch = bcrypt.compare(req.body.password, user.password);
-          if (!isMatch) {
-            return res.status(401).send("Invalid email or password");
-          }
-          else {
-            const token = jwt.sign(
-              { _id: user._id, role: user.role },
-              process.env.JWT_SECRET
-            );
-            return res.header("auth-token", token).send(token);
-          }
-        }
-      })
-      .catch((err) => console.log(err));
+
+  //checking whether the user already exists in the database
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.send("Account with the given Email does  not exist");
   }
+
+  //Checking password
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid password");
+
+  const token = jwt.sign(
+    { _id: user._id, role: user.role },
+    process.env.JWT_SECRET
+  );
+  // const token = jwt.sign(payload, process.env.JWT_SECRET, {expiresIn: '1d'})
+  res.header("auth-token", token).send(token);
+}catch(error){
+  console.log(error)
+}
 };
 
 export const logout = async (req, res) => {
   try {
-    res.clearCookie('auth-token');
-    res.status(200).send('You have been logged out');
-  } catch (err) {
-    res.status(500).send(err.message);
+    res.setHeader('auth-token', '');
+    res.status(200).send('User successfully logged out');
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('Server Error');
   }
 };
--- controllers/authController.test.js
+++ controllers/authController.test.js
@@ -2,109 +2,152 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 import { registerValidation, loginValidation } from "../utils/validation.js";
-import {register, login} from "./authController.js";
-// Mock the User model and its methods
-jest.mock("../models/User.js", () => ({
-  findOne: jest.fn(),
-  save: jest.fn(),
-}));
+import { register, login, logout } from "../controllers/authController.js";
 
-describe("register", () => {
-  it("should return 400 if input is invalid", async () => {
-    const req = { body: {} };
+describe("register function", () => {
+  it("should return 400 if invalid data is provided", async () => {
+    const req = {
+      body: {
+        name: "student",
+        email: "example@gmail.com",
+        password: "123456",
+      },
+    };
     const res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn(),
     };
-
     await register(req, res);
-
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.send).toHaveBeenCalled();
+    // expect(res.send).toHaveBeenCalledWith(
+    //   "Validation failed: email must be a valid email"
+    // );
   });
 
-  it("should return 409 if email already exists", async () => {
-    const req = { body: { email: "test@example.com", password:"Abcdef@1" } };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn(),
+  it("should return 400 if user with email already exists", async () => {
+    const req = {
+      body: {
+        name: "teststudent1",
+        email: "teststudent1@gmail.com",
+        password: "Abcedf@1",
+      },
     };
-    const user = { email: "test@example.com" };
-
-    User.findOne.mockResolvedValue(user);
-
-    await register(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(409);
-    expect(res.send).toHaveBeenCalled();
-  });
-
-  it("should create a new user and return a success message", async () => {
-    const req = { body: { name: "Test User", email: "test@example.com", password: "password" } };
     const res = {
       send: jest.fn(),
     };
-    const salt = "salt";
-    const hash = "hash";
-    const user = { _id: "123", name: "Test User", email: "test@example.com" };
-
-    bcrypt.genSalt.mockResolvedValue(salt);
-    bcrypt.hash.mockResolvedValue(hash);
-    User.findOne.mockResolvedValue(null);
-    User.save.mockResolvedValue(user);
-
+    User.findOne = jest.fn(() => Promise.resolve({ email: req.body.email }));
     await register(req, res);
+    expect(res.send).toHaveBeenCalledWith("Email already exists");
+  });
 
-    expect(User.findOne).toHaveBeenCalledWith({ email: req.body.email });
-    expect(bcrypt.genSalt).toHaveBeenCalledWith(10);
-    expect(bcrypt.hash).toHaveBeenCalledWith(req.body.password, salt);
-    expect(User.save).toHaveBeenCalled();
-    expect(res.send).toHaveBeenCalledWith({ user: user._id, message: "User created" });
+  it("should create a new user with hashed password and return user id and message", async () => {
+    const req = {
+      body: {
+        name: "teststudent2",
+        email: "teststudent2@gmail.com",
+        password: "Abcedf@1",
+      },
+    };
+    const res = {
+      send: jest.fn(),
+    };
+    User.findOne = jest.fn(() => Promise.resolve(null));
+    bcrypt.genSalt = jest.fn(() => Promise.resolve("salt"));
+    bcrypt.hash = jest.fn(() => Promise.resolve("hashedPassword"));
+    const saveMock = jest.fn(() =>
+      Promise.resolve({ _id: "user_id", name: "John", email: "john@gmail.com" })
+    );
+    User.prototype.save = saveMock;
+    await register(req, res);
+    expect(saveMock).toHaveBeenCalled();
+    expect(res.send).toHaveBeenCalledWith({
+      user: "user_id",
+      message: "User created",
+    });
   });
 });
 
-describe("login", () => {
-  it("should return 400 if input is invalid", async () => {
-    const req = { body: {} };
+describe("login function", () => {
+  it("should return 400 if invalid data is provided", async () => {
+    const req = {
+      body: {
+        email: "wrongemail.example.com",
+        password: "Abcdef@1",
+      },
+    };
     const res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn(),
     };
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith("Enter a valid email address");
+  });
+
+  it("should return 400 if user with email does not exist", async () => {
+    const req = {
+      body: {
+        email: "doesNotExist@example.com",
+        password: "Abcdef@1",
+      },
+    };
+    const res = {
+      status:jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    User.findOne = jest.fn(() => Promise.resolve(null));
+    await login(req, res);
+    expect(res.send).toHaveBeenCalledWith(
+      "Account with the given Email does  not exist"
+    );
+  });
+
+  it("should return 400 if invalid password is provided", async () => {
+    const req = {
+      body: {
+        email: "teststudent1@gmail.com",
+        password: "Abcedf@01",
+      },
+    };
+    const res = {
+      send: jest.fn(),
+      status: jest.fn().mockReturnThis(),
+    };
+    User.findOne = jest.fn(() => Promise.resolve({ password: "hashedPassword" }));
 
     await login(req, res);
 
     expect(res.status).toHaveBeenCalledWith(400);
-    expect(res.send).toHaveBeenCalled();
-  });
+    expect(res.send).toHaveBeenCalledWith("Invalid password");
+});
 
-  it("should return 401 if user doesn't exist", async () => {
-    const req = { body: { email: "test@example.com", password: "password" } };
+});
+
+
+describe('logout controller', () => {
+  it('should clear the auth-token header and return a 200 status code with success message', async () => {
+    const req = {};
     const res = {
+      setHeader: jest.fn(),
       status: jest.fn().mockReturnThis(),
       send: jest.fn(),
     };
-
-    User.findOne.mockResolvedValue(null);
-
-    await login(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(401);
-    expect(res.send).toHaveBeenCalledWith("Invalid email or password");
+    await logout(req, res);
+    expect(res.setHeader).toHaveBeenCalledWith('auth-token', '');
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith('User successfully logged out');
   });
 
-  it("should return 401 if password is incorrect", async () => {
-    const req = { body: { email: "test@example.com", password: "password" } };
+  it('should return a 500 status code with an error message if an error occurs', async () => {
+    const req = {};
     const res = {
+      setHeader: jest.fn(() => { throw new Error() }),
       status: jest.fn().mockReturnThis(),
       send: jest.fn(),
     };
-    const user = { password: "wrong_password" };
+    await logout(req, res);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith('Server Error');
+  });
+});
 
-    User.findOne.mockResolvedValue(user);
-    bcrypt.compare.mockResolvedValue(false);
-
-    await login(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(401);
-  })
-})
