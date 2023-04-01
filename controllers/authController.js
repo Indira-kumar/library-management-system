4566b0bc1301b3695ecae0580dc674e7461a47a9
Indira Kumar <indirakumarak@gmail.com>
2023-04-01 18:45:24 +0530

    add logout feature

--- controllers/authController.js
+++ controllers/authController.js
@@ -68,3 +68,12 @@ export const login = async (req, res) => {
       .catch((err) => console.log(err));
   }
 };
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('auth-token');
+    res.status(200).send('You have been logged out');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
--- router/authRoute.js
+++ router/authRoute.js
@@ -1,8 +1,9 @@
 import {Router} from "express";
-import {register, login} from "../controllers/authController.js"
+import {register, login, logout} from "../controllers/authController.js"
 
 const authRouter = Router();
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.delete("/logout", logout);
 
 export default authRouter;
