d7ab8dc804917d6d9c3e7c0a5667c0e9d24e5772
Indira Kumar <indirakumarak@gmail.com>
2023-04-02 03:12:22 +0530

    add feature user logout

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,10 @@
 import {Router} from "express";
-import { register } from "../controllers/userController";
+import {register, login, logout} from "../controllers/userController.js"
+import {verifyToken} from "../middlewares/verifyToken.js";
 
 const userRouter = Router();
 userRouter.post("/register", register);
 userRouter.post("/login", login);
+authRouter.delete("/logout",verifyToken, logout);
 
 export default userRouter;
