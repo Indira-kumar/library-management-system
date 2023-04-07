977cb1f9296cde4f2cd3446f4bf64b8c5032dcdb
Keshav Rathinavel <73035121+keshavrathinavel@users.noreply.github.com>
2023-04-07 16:01:12 +0530

    resolves issue #10, #11, #12 (#27)

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -38,6 +38,68 @@ export const findBook = async (req, res) => {
       }
 }
 
+export const borrowBook = (req, res) => {
+  const { userId } = req.user._userid;
+  const { ISBN } = req.body.ISBN;
+  
+  // update existing table by decrementing qty by 1 and adding user to the user field
+  Book.findOneAndUpdate(
+    { ISBN, quantity: { $gt: 0 } },
+    { $addToSet: { users: userId }, $inc: { quantity: -1 } },
+    { new: true }
+  )
+    .then((book) => {
+      if (!book) {
+        return res.status(404).send("Book not available");
+      }
+      res.status(200).send(book);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+};
+
+export const checkBorrowed = (req, res) => {
+  console.log(req.user)
+  const { userId } = req.user._userid;
+  const { ISBN } = req.body.ISBN;
+
+  Book.findOne({ ISBN: bookISBN })
+    .populate("users", "_id") 
+    .then((book) => {
+      if (!book) {
+        return res.status(404).send("Book not found");
+      }
+      const borrowedByUser = book.users.some((user) => user._id.equals(userId));
+      res.status(200).send({ borrowed: borrowedByUser });
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+};
+
+export const returnBook = (req, res) => {
+  const { userId } = req.user._userid;
+  const { ISBN } = req.body.ISBN;
+ 
+  // increment qty by 1 and remove user using pull function
+  Book.findOneAndUpdate(
+    { ISBN: bookISBN, users: userId },
+    { $pull: { users: userId }, $inc: { quantity: 1 } }, 
+    { new: true }
+  )
+    .then((book) => {
+      if (!book) {
+        return res.status(404).send("Book not found for user");
+      }
+      res.status(200).send(book);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+};
+
+
 
 export const deleteBook = async (req, res) => {
       try {
--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-import { findAllBooks, insertBook, findBook, deleteBook, updateBook } from "../controllers/bookController.js";
+import { findAllBooks, insertBook, findBook, deleteBook, updateBook, borrowBook, checkBorrowed, returnBook } from "../controllers/bookController.js";
+import { verify } from "jsonwebtoken";
 
 const bookRoute = Router();
 
 bookRoute.post("/", insertBook);
 bookRoute.get("/", findAllBooks);
 bookRoute.get("/:isbn", findBook);
+bookRoute.put("/", verify, borrowBook);
+bookRoute.get("/", checkBorrowed);
+bookRoute.put("/", returnBook);
 bookRoute.delete("/:isbn", deleteBook);
 bookRoute.patch("/:isbn", updateBook);
 
